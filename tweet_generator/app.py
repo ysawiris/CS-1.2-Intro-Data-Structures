2865034dc33fc81c1cde70c1b31f8d38185f400c
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-11-06 11:04:24 -0800

    heroku updates

--- tweet_generator/app.py
+++ tweet_generator/app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Lryic-Generator')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/create-lryic-gen')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 lyric = db['lyric']
@@ -24,4 +24,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
