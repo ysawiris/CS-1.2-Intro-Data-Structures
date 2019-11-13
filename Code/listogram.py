921ae3d69b117b88c2a11928993b3642f5a79e91
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-11-13 11:29:47 -0800

    fixed listogram errors

--- Code/listogram.py
+++ Code/listogram.py
@@ -1,7 +1,7 @@
-#!python
 
+#!python
 from __future__ import division, print_function  # Python 2 and 3 compatibility
-import random
+import random 
 
 
 class Listogram(list):
@@ -10,9 +10,11 @@ class Listogram(list):
     def __init__(self, word_list=None):
         """Initialize this histogram as a new list and count given words."""
         super(Listogram, self).__init__()  # Initialize this as a new list
+
         # Add properties to track useful word counts for this histogram
         self.types = 0  # Count of distinct word types in this histogram
         self.tokens = 0  # Total count of all word tokens in this histogram
+
         # Count words in given list, if any
         if word_list is not None:
             for word in word_list:
@@ -20,8 +22,6 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
-        #check if the word is already in the histogram 
         index = self.index_of(word)
         if index is not None:
             old_count = self[index][1]
@@ -32,46 +32,46 @@ class Listogram(list):
             self.types += 1
         self.tokens += count
 
-
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
-        for index, item in enumerate(self):
-            if item[0] == word:
-                return item[1]
-            else:
-                return 0 
+        #loop through the histogram 
+        for count in self:
+            #check if the word is in histogram 
+            if count[0] == word:
+                #retrun the count 
+                return count[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
-        for index, item  in enumerate(self):
-            if self[item][0] == word:
+        #loop through the histogram 
+        for text in self:
+            #check if text is in histogram 
+            if word == text[0]:
                 return True
-            else:
-                return False
+        return False
+
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
-        # TODO: Implement linear search to find index of entry with target word
-        for index, item in enumerate(self):
-            if item[0] == target:
-                return index
-            else: 
-                return None 
+        #loop through the histogram 
+        for word in self:
+            #check if target is in histogram 
+            if target == word[0]:
+                #return the index 
+                return self.index(word)
+        return None
+
     def sample(self):
-        """Return a word from this histogram, randomly sampled by weighting
-        each word's probability of being chosen by its observed frequency."""
-        # TODO: Randomly choose a word based on its frequency in this histogram
-        count = 0 
 
-        #pick a random number between 
-        random_index = random.randint(0, self.tokens-1)
+        count = random.randint(1, self.tokens)
+        for word in self:
+            count -= word[1]
+            if count <= 0:
+                return word[0]
+        return -1
 
-        for index, item in enumerate(self):
-            count += item[1]
-            if count > random_index:
-                return item[0]
+
 
 def print_histogram(word_list):
     print()
